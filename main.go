15d74631262902a52927139c30c0046c056dd8a1
khuyentv <khuyentv@khuyentvs-Mac-mini.local>
2025-12-25 17:22:40 +0700

    change print message

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var (
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte(fmt.Sprintf("ping ok %s", ServiceName)))
+		w.Write([]byte(fmt.Sprintf("ping vcl ok %s", ServiceName)))
 	})
 	fmt.Println("start service with port: ", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
