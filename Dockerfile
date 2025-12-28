da81f1463af17d8d292ee229bec2dbd9821dee5b
khuyentv <khuyen.dev183@gmail.com>
2025-12-29 01:59:29 +0700

    config docker compose

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ WORKDIR /app
 COPY . .
 RUN go mod download
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o /out/main ./
+ENV API_RES=ok
 ENTRYPOINT ["/out/main"]
 
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func init() {
-
+	godotenv.Load()
 }
 
 var (
@@ -16,8 +19,9 @@ var (
 )
 
 func main() {
-	http.HandleFunc("/ngonlun", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte(fmt.Sprintf("ping vcl ok %s", ServiceName)))
+	http.HandleFunc("/ngonluon", func(w http.ResponseWriter, req *http.Request) {
+		apiRes := os.Getenv("API_RES")
+		w.Write([]byte(apiRes))
 	})
 	fmt.Println("start service with port: ", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
